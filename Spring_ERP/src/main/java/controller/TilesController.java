109aad9f1b260ef15499430c349f6a19d8ce38e8
Kang_Mingu <jkang4531@naver.com>
2024-09-26 03:24:29 +0900

    tiles 설정 안됨

--- Spring_ERP/src/main/java/controller/TilesController.java
+++ Spring_ERP/src/main/java/controller/TilesController.java
@@ -7,11 +7,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TilesController {
     @RequestMapping("/")
     public String tiles() {
+        // 템플릿 요청 경로 아닌가..?
         return "main";
     }
 
     @RequestMapping("/admin")
     public String admin() {
         return "admin";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
     }
 }

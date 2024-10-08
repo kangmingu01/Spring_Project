9243b430b80d0a38ed127ef1ba84edb687e4afd8
itwill <itwill@DESKTOP-LS2VI2J>
2024-10-08 15:06:52 +0900

    .

--- Spring_ERP/src/main/java/controller/ProductController.java
+++ Spring_ERP/src/main/java/controller/ProductController.java
@@ -1,6 +1,6 @@
 package controller;
 
-import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dto.Product;
@@ -21,25 +22,53 @@ import service.ProductService;
 public class ProductController {
 	private final ProductService productService;
 	
-	@PostMapping("/add")
+//	@GetMapping("/product_list")
+//	public List<Product> productListAll() {
+//		return productService.getProductListAll();
+//	}
+	/*
+	@GetMapping("/product_list")
+	public Map<String, Object> getproductKeyList(@RequestParam(defaultValue = "1") int pageNum
+			, @RequestParam(defaultValue = "10") int pageSize
+			, @RequestParam(defaultValue = "") String search
+			, @RequestParam(defaultValue = "") String keyword
+			, @RequestParam Map<String, Object> map) {
+		
+		map.put("pageNum", String.valueOf(pageNum));  
+	    map.put("pageSize", String.valueOf(pageSize));
+	    map.put("search", search);
+	    map.put("keyword", keyword);
+		
+	    return productService.getselectProductKeyList(map);
+	}
+	*/
+	@GetMapping("/product_list")
+	public Map<String, Object> getproductKeyList(@RequestParam Map<String, Object> map) {
+		return productService.getselectProductKeyList(map);
+	}
+	
+	@PostMapping("/product_add")
 	public String productAdd(@RequestBody Product product) {
 		productService.addProduct(product);
 		return "success";
 	}
-	@PutMapping("/modify")
+	
+	@GetMapping("/product_modify_view/{productId}")
+	public Product productModifyView(@PathVariable int productId) {
+		return productService.getProductUpdate(productId);
+	}
+	
+	@PutMapping("/product_modify")
 	public String productModify(@RequestBody Product product) {
 		productService.modifyProduct(product);
 		return "success";
 	}
 	
-	@DeleteMapping("/remove/{idx}")
+	@DeleteMapping("/product_remove/{idx}")
 	public String productRemvoe(@PathVariable int idx) {
 		productService.removeProduct(idx);
 		return "success";
 	}
 	
-	@GetMapping("/list")
-	public List<Product> productView() {
-		return productService.getProductListAll();
-	}
+	
 }
--- Spring_ERP/src/main/java/mapper/ProductMapper.java
+++ Spring_ERP/src/main/java/mapper/ProductMapper.java
@@ -1,6 +1,7 @@
 package mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import dto.Product;
 
@@ -8,5 +9,8 @@ public interface ProductMapper {
 	int insertProduct(Product product);
 	int updateProduct(Product product);
 	int deleteProduct(int productId);
+	Product selectProductUpdate(int productId );
 	List<Product> selectProductAll();
+	int selcetProductCount(Map<String, Object> map);
+	List<Product> selectProductKeyList(Map<String, Object> map);
 }
